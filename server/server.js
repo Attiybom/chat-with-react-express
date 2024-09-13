6cbac2d4f0366f963580543c21e223180517d68a
attiybom <a910667665@gmail.com>
2024-09-13 19:59:47 +0800

    feat: backend send num to client

--- server/server.js
+++ server/server.js
@@ -1,22 +1,61 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io'
-import cors from 'cors';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `http://localhost:3000`,
-    methods: ['GET', 'POST']
+    origin: `http://localhost:5173`,
+    methods: ["GET", "POST"],
+  },
+});
+
+let timer;
+let clientsCount = 0;
+
+const startTimer = () => {
+  if (!timer) {
+    timer = setInterval(() => {
+      const count = Math.floor(Math.random() * 100) + 1;
+      io.emit("message_response", { count });
+    }, 2000);
   }
-})
+}
+
+const stopTimer = () => {
+  if (timer && clientsCount === 0) {
+    clearInterval(timer);
+    timer = null;
+  }
+ }
+
+io.on("connection", (socket) => {
+  // 连接成功
+  console.log(`a user:${socket.id} connected`);
+
+  // 接收消息
+  socket.on("new_message", (data) => {
+    console.log("New message received ", data);
+  });
+
+  startTimer();
+
+
+  // 清除定时器
+  socket.on("disconnect", () => {
+    console.log(`user:${socket.id} disconnected`);
+    clientsCount--;
+    stopTimer();
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(PORT, () => {
+  console.log(`listening on *: ${PORT}`);
 });
